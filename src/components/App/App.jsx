eef4bc05bfe660b4c34c0205b4cd372c33742709
Maker0N <makerov@e1.ru>
2021-11-28 20:35:14 +0500

    main, header, flats, flat info

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Main from '../Main/Main'
+import Header from '../Header/Header'
+import '../../styles/main.css'
+
+const App = () => (
+  <main className="main">
+    <Header />
+    <Main />
+  </main>
+)
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => (
+  <header className="header">
+    <div>
+      ТСЖ и ТСЖ
+    </div>
+    <div>
+      Вы вошли под акаунтом:
+    </div>
+  </header>
+)
+
+export default Header
